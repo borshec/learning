377567dd6156d7c7a5a3273fa095b65746e9ccf8
borsh <young.specialist@gmail.com>
2017-05-11 22:57:38 +0300

    ordinary changes

--- python/module2.py
+++ python/module2.py
@@ -1,5 +1,6 @@
 a = 'test1'
-b = 'test2'
+b = 'test2i'
 print(a+b)
 print(a+str(5))
 print(a*5)
+print(2**40)
---
+++ python/temp.py
@@ -0,0 +1,15 @@
+l1 = [1, 2, 'a']
+print(l1)
+print(type(l1))
+
+s1 = '1sdhdhdbsb'
+print(s1)
+print(type(s1))
+print(s1.find('h'))
+
+s2 = 'd'
+i1 = ord(s2)
+print(i1)
+
+print(ord('z'))
+print(chr(122))
